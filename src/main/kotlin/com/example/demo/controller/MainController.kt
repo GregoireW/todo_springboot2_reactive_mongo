d45603ff3081a004bc95754dd13cb48365052ea7
gregoirew <gregoirew@users.noreply.github.com>
2017-06-05 20:32:58 +0200

    Fix 404 error if not found.

--- src/main/kotlin/com/example/demo/controller/MainController.kt
+++ src/main/kotlin/com/example/demo/controller/MainController.kt
@@ -4,6 +4,7 @@ import com.example.demo.Config
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -21,14 +22,18 @@ data class Todo(@Id var id:Long=0, var title:String="", var completed:Boolean=fa
 }
 
 
-interface ReactiveTodoRepository: ReactiveCrudRepository<Todo, Long> {
+interface ReactiveTodoRepository: ReactiveCrudRepository<Todo, Long>
 
-}
-
+class EmptyException : Throwable()
 
 @RestController
 @CrossOrigin
 class MainController(val todoRepository: ReactiveTodoRepository) {
+
+    @ExceptionHandler(EmptyException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun err404()=Mono.empty<Void>();
+
 
     @GetMapping("/")
     fun listTodo(): Flux<Todo> = todoRepository.findAll();
@@ -44,7 +49,9 @@ class MainController(val todoRepository: ReactiveTodoRepository) {
     
 
     @GetMapping("/{id}")
-    fun getTodo(@PathVariable("id") id: Long): Mono<Todo> = todoRepository.findById(id)
+    fun getTodo(@PathVariable("id") id: Long) = todoRepository.findById(id).
+            switchIfEmpty(Mono.error(EmptyException()))
+
 
     @DeleteMapping("/{id}")
     fun remove(@PathVariable("id") id: Long)=todoRepository.deleteById(id)
@@ -52,7 +59,9 @@ class MainController(val todoRepository: ReactiveTodoRepository) {
 
     @PatchMapping("/{id}")
     fun update(@PathVariable("id") id: Long, @RequestBody todo: Mono<Todo>): Mono<Todo>
-        = todoRepository.findById(id).and(todo).map {
+        = todoRepository.findById(id).
+            switchIfEmpty(Mono.error(EmptyException())).
+            and(todo).map {
             if (!it.t2.title.isEmpty()) it.t1.title=it.t2.title
             if (it.t2.completed) it.t1.completed=true
             if (it.t2.order>-1) it.t1.order=it.t2.order
